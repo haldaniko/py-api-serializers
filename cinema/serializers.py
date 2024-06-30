fd4ff140407aaf193acd419097723824e6045684
haldaniko <haldaniko@gmail.com>
2024-06-30 16:08:29 +0200

    fix flake8

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -14,7 +14,7 @@ class CinemaHallSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CinemaHall
-        fields = ("id", "name", "rows", "seats_in_row", "capacity",)
+        fields = ("id", "name", "rows", "seats_in_row", "capacity")
 
 
 class GenreSerializer(serializers.ModelSerializer):
--- cinema/urls.py
+++ cinema/urls.py
@@ -16,7 +16,9 @@ router.register("cinema_halls", CinemaHallViewSet, basename="cinema-hall")
 router.register("genres", GenreViewSet, basename="genre")
 router.register("actors", ActorViewSet, basename="actor")
 router.register("movies", MovieViewSet, basename="movie")
-router.register("movie_sessions", MovieSessionViewSet, basename="movie-session")
+router.register("movie_sessions",
+                MovieSessionViewSet,
+                basename="movie-session")
 router.register("orders", OrderViewSet, basename="order")
 router.register("tickets", TicketViewSet, basename="ticket")
 
