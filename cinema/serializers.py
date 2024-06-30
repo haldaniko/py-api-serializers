658767edd3748b6dce17888c10dcfc378af73775
haldaniko <haldaniko@gmail.com>
2024-06-30 13:41:04 +0200

    add simple class-based serializers for all models

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,1 +1,51 @@
-# write serializers here
+from rest_framework import serializers
+
+from cinema.models import (CinemaHall,
+                           Genre,
+                           Actor,
+                           Movie,
+                           MovieSession,
+                           Order,
+                           Ticket)
+
+
+class CinemaHallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CinemaHall
+        fields = ("name", "rows", "seats_in_row")
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ("name",)
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = ("first_name", "last_name",)
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ("show_time", "movie", "cinema_hall")
+
+
+class MovieSessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MovieSession
+        fields = ("show_time", "movie", "cinema_hall")
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ("created_at", "user")
+
+
+class TicketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = ("movie_session", "order", "row", "seat")
