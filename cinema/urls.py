3994bd54d5dbbfbbfddc0737bd85474dd9340a8a
haldaniko <haldaniko@gmail.com>
2024-06-30 13:51:32 +0200

    add urls for all views

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,25 @@
-# write urls here
+from django.urls import path, include
+from rest_framework import routers
+from cinema.views import (CinemaHallViewSet,
+                          GenreViewSet,
+                          ActorViewSet,
+                          MovieViewSet,
+                          MovieSessionViewSet,
+                          OrderViewSet,
+                          TicketViewSet)
+
+app_name = "cinema"
+
+router = routers.DefaultRouter()
+
+router.register("halls", CinemaHallViewSet, basename="cinema-hall")
+router.register("genres", GenreViewSet, basename="genre")
+router.register("actors", ActorViewSet, basename="actor")
+router.register("movies", MovieViewSet, basename="movie")
+router.register("sessions", MovieSessionViewSet, basename="movie-session")
+router.register("orders", OrderViewSet, basename="order")
+router.register("tickets", TicketViewSet, basename="ticket")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
